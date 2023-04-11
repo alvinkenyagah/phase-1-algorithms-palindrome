3e98b9c967a3dfb2808bd2eff77dfeaf6553caaf
alvinkenyagah <alvin47k@gmail.com>
2023-04-11 14:35:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let splitWord = word.split("");
+  let reversedArray = splitWord.reverse();
+  let reversedWord = reversedArray.join("");
+
+  return reversedWord === word;
 }
+isPalindrome("ab");
+isPalindrome("a");
+isPalindrome("abba");
+isPalindrome("racecar");
+isPalindrome("robot");
 
 /* 
   Add your pseudocode here
+
+  Function isPalindrome(word):
+splitWord = split word into an array of characters
+reversedArray = reverse the order of characters in splitWord
+reversedWord = join characters in reversedArray into a single string
+return true if reversedWord is equal to word, false otherwise.
+
+
 */
 
 /*
   Add written explanation of your solution here
+
+isPalindrome that takes in a single parameter called word.
+The function first splits the input word into an array of its characters using the split() method and stores it in a variable called splitWord.
+It then reverses the order of the characters in splitWord using the reverse() method and stores it in a variable called reversedArray.
+Finally, it joins the characters in reversedArray into a single string using the join() method and stores it in a variable called reversedWord.
+
+
 */
 
 // You can run `node index.js` to view these console logs
